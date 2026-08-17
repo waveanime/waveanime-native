b8c756ac7bfb4a5df5f155734e819a3a62129224
Gamatek <mathieu.bontemps3.0@gmail.com>
2026-08-17 17:59:08 +0200

    feat(pub): add http and flutter_secure_storage feat(android): add internet permission and update compile sdk version

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import dynamic_color
+import flutter_secure_storage_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
 }
